36d68b1852b5a3aebab92e51ff89c73760628b10
Ketopluto <150611515+Ketopluto@users.noreply.github.com>
2025-10-15 19:29:33 +0530

    Damnn

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from app import app
 
 # This is the WSGI entry point for Vercel
-def handler(request, context):
-    return app(request.environ, context)
+# def handler(request, context):
+#     return app(request.environ, context)
 
-# For local testing
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+# # For local testing
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5000)
