12cfb6908bc058697ff8f21daf44f9b776edfe51
VISVESH SANATHAN A <150611515+Ketopluto@users.noreply.github.com>
2025-10-22 08:14:24 +0530

    Delete app directory

--- app/assets/convert_icon.py
+++
@@ -1,6 +0,0 @@
-from PIL import Image
-
-# Convert PNG to ICO
-img = Image.open('leetcode_icon.png')
-icon_sizes = [(16,16), (32,32), (48,48), (64,64), (128,128), (256,256)]
-img.save('leetcode-icon.ico', sizes=icon_sizes)
